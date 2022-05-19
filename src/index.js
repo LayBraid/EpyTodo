8ae178b8f9e2b227cad941cc015ab93a22e88701
LayBraid <clement.loeuillet@epitech.eu>
2022-05-19 17:52:25 +0200

    :construction: add auth to function

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const port = 3000
 const user = require('./routes/user/user.js')
 const todo = require('./routes/todos/todos.js')
 
+const auth = require('../../middleware/auth');
+
 var bodyParser = require('body-parser')
 
 app.use(express.raw());
@@ -13,40 +15,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 
-app.post('/register', user.register)
+app.post('/register', auth, user.register)
+app.delete('/user/:id', auth, user.deleteUser)
 
-app.delete('/user/:id', user.deleteUser)
-
-app.post('/todos', todo.addTodoPlayer)
-
-app.get('/todos', todo.getAllTodosList)
-
-app.delete('/todos/:id', todo.delTodoById)
-
-app.get('/todos/:id', todo.getTodoById)
-
-app.put('/todos/:id', todo.updateTodoById)
-
-/*app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({'msg':'Hello World!'}))
-})
-
-app.get('/user', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    db.query("SELECT * FROM user", function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        res.send(result)
-    });
-})
-
-app.post('/user', (req, res) => {
-    db.query("INSERT INTO user (email, password, name, firstname) values ('agherasie@epitech.eu', 'pass', 'gherasie', 'alex');", function (err, result, fields) {
-        if (err) throw err;
-        res.send(result);
-    })
-})*/
+app.post('/todos', auth, todo.addTodoPlayer)
+app.get('/todos', auth, todo.getAllTodosList)
+app.delete('/todos/:id', auth, todo.delTodoById)
+app.get('/todos/:id', auth, todo.getTodoById)
+app.put('/todos/:id', auth, todo.updateTodoById)
 
 app.listen(port, () => {
     console.log('Example app listening on port ' + port)
